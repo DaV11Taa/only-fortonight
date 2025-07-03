bbb10a7ee2153f0ecd02874a8256adc54a92a92d
Gvancacuna <gvancabegiashvili@gmail.com>
2025-07-03 21:58:59 +0400

    sul importi rava maviwydeba

--- src/ShippinhInfoPage/ConfirmationPage/ShipingPayment.jsx
+++ src/ShippinhInfoPage/ConfirmationPage/ShipingPayment.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import ShippingPayment from "./ShippingPayment.module.css"
+import { useNavigate } from "react-router-dom";
 const ShipingPayment = () => {
   const [orderNumber, setOrderNumber,setCartItems] = useState(1);
   const navigate = useNavigate();
